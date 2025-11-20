8ab0851b07a29859604eb710ab0b8b543c8c2a3f
saif <frazmdsaif@gmail.com>
2025-11-20 11:01:55 +0530

    test-branch

--- src/test/java/org/elivaas/utils/BrowserManager.java
+++ src/test/java/org/elivaas/utils/BrowserManager.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import java.io.IOException;
 
 public class BrowserManager {
+    //test
 
     public static WebDriver doBrowserSetup(boolean headless) throws IOException {
         String name=PropertiesLoader.loadProperty("browser.name");
