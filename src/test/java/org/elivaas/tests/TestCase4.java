d17aba4900aa2029c9b8460b4c9e87edd05489c9
saif <frazmdsaif@gmail.com>
2025-12-11 14:19:29 +0530

    new test cases added

--- src/test/java/org/elivaas/tests/TestCase4.java
+++ src/test/java/org/elivaas/tests/TestCase4.java
@@ -1,11 +1,35 @@
 package org.elivaas.tests;
 
+import org.elivaas.pages.ListingPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.List;
 
 public class TestCase4 extends TestBasic{
 
     @Test(description = "Verify that property page is visible successfully")
-    public void verifyThatPropertyPageIsVisible() {
+    public void verifyThatPropertyPageIsVisible() throws InterruptedException, IOException {
+        ListingPage listingPage=new ListingPage(getDriver());
+        listingPage.openListingPageViaTest();
+        List<WebElement> cards=getDriver().findElements(By.xpath("//*[@data-slot='card']"));
+        String currentWindow=getDriver().getWindowHandle();
+        System.out.println(currentWindow);
 
-    }`
+        if(!cards.isEmpty()){
+            WebElement card=cards.get(0);
+            if(card.isDisplayed()){
+                card.click();
+                Thread.sleep(2000);
+                System.out.println(getDriver().getCurrentUrl());
+            }
+        }
+
+
+
+    }
+
+
 }
