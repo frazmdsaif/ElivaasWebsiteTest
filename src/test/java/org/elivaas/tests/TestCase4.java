0b03791ed59e013ad035cf808e975edc87a84a6c
saif <frazmdsaif@gmail.com>
2026-02-03 23:26:41 +0530

    test 4 added

--- src/test/java/org/elivaas/tests/TestCase4.java
+++ src/test/java/org/elivaas/tests/TestCase4.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Set;
 
 public class TestCase4 extends TestBasic{
 
@@ -16,14 +17,22 @@ public class TestCase4 extends TestBasic{
         listingPage.openListingPageViaTest();
         List<WebElement> cards=getDriver().findElements(By.xpath("//*[@data-slot='card']"));
         String currentWindow=getDriver().getWindowHandle();
-        System.out.println(currentWindow);
+
 
         if(!cards.isEmpty()){
             WebElement card=cards.get(0);
             if(card.isDisplayed()){
                 card.click();
                 Thread.sleep(2000);
+            }
+        }
+        Set<String> totalWindow=getDriver().getWindowHandles();
+        for(String window:totalWindow){
+            if(!window.equals(currentWindow)){
+                getDriver().switchTo().window(window);
                 System.out.println(getDriver().getCurrentUrl());
+                Thread.sleep(2000);
+                getDriver().close();
             }
         }
 
